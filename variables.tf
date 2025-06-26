487287d9ea963d740faea0d64f3b86571cff0822
vaeshwar <Eshwarchitti1212@outlook.com>
2025-06-26 12:36:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,19 @@
 variable "region" {
-  description = "AWS region to deploy instances"
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "key_name" {
-  description = "EC2 Key Pair Name"
-  default     = "jenkins"
-}
-
-variable "security_group_id" {
-  description = "Security Group ID"
-  default     = "sg-0d7f8155c7d09c24d"
+  default = "jenkins"
 }
 
 variable "ami_amazon_linux" {
-  description = "Amazon Linux 2 AMI ID"
-  default     = "ami-0f3f13f145e66a0a3"
+  default = "ami-0f3f13f145e66a0a3" # Amazon Linux 2023
 }
 
 variable "ami_ubuntu" {
-  description = "Ubuntu 24.04 AMI ID"
-  default     = "ami-020cba7c55df1f615"
+  default = "ami-020cba7c55df1f615" # Ubuntu 24.04
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
+  default = "t2.micro"
 }
